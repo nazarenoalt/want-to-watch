197fb1f6b57786bc3584c8bcd6289164391b2695
nazarenoalt <nazalt256@gmail.com>
2021-11-23 10:27:52 -0300

    [UPDATE] useMovieFetch hook

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 // config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 // components
 import Grid from '../components/Grid'
 import Spinner from '../components/Spinner'
+// hooks
+import { useMovieFetch } from '../hooks/useMovieFetch'
 // image
 import NoImage from '../images/no_image.jpg'
 
 const Movie = () => {
+  const { movieId  } = useParams()
+  const { state, loading, error } = useMovieFetch(movieId)
+
+  console.log(state)
   return (
     <>
       <div>Movie</div>
