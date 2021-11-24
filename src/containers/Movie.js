7d88715813f2465e540ceb0c6dc3aa3b0ab3c68a
nazarenoalt <nazalt256@gmail.com>
2021-11-23 21:11:28 -0300

    [UPDATE] finished BreadCrumb and MovieInfo logic

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -3,21 +3,26 @@ import { useParams } from 'react-router-dom'
 // config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 // components
-import Grid from '../components/Grid'
 import Spinner from '../components/Spinner'
 // hooks
 import { useMovieFetch } from '../hooks/useMovieFetch'
 // image
 import NoImage from '../images/no_image.jpg'
+import BreadCrumb from '../components/BreadCrumb'
+import MovieInfo from '../components/MovieInfo'
 
 const Movie = () => {
   const { movieId  } = useParams()
-  const { state, loading, error } = useMovieFetch(movieId)
-
-  console.log(state)
+  const { state: movie, loading, error } = useMovieFetch(movieId)
+  console.log(movie.original_title)
+  if(loading) return <Spinner />
+    
+  if(error) return <div>Something went wrong. Try again</div>
+  
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title}/>
+      <MovieInfo movie={movie} />
     </>
   )
 }
